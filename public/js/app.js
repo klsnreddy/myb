15186667946a564fd1488a1aafa863467cd5e61f
klsnreddy <klsnreddy@gmail.com>
2016-01-28 21:01:38 -0500

    Adding public

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+/*angular.module('TestApp', []);
+
+angular.module('TestApp')
+	.controller('MainController', ctrlFunc);
+	
+function ctrlFunc() {
+	this.people = clientPeople;
+}*/
+
+// This code only runs on the client
+
+  angular.module('simple-todos',[]);
+ 
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
+    function ($scope) {
+ 
+      $scope.tasks = [
+        { text: 'This is task 1' },
+        { text: 'This is task 2' },
+        { text: 'This is task 3' }
+      ];
+ 
+  }]);

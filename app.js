4acc14463871b6b515e4601b9019b1db9147af36
klsnreddy <klsnreddy@gmail.com>
2016-01-31 04:03:50 +0000

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var ejs = require("ejs");
-    
+var db = require('./lib/db');
+var mongoose = require('mongoose');  
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -21,10 +22,19 @@ app.engine("html", ejs.renderFile);
 app.set('view engine', 'html');
 app.use('/assets', express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://localhost:27017/myb');
+var Schema = mongoose.Schema;
+var tasksSchema = new Schema({
+    text: String
+});
+var tasks = mongoose.model('tasks', tasksSchema);
+
 app.get('/', function(req, res) {
-	
-	res.render('index', { serverPeople: people });
-	
+    tasks.find({}, function(err, tsks) {	
+      if(err) console.log(err);
+      console.log(tsks);
+      res.render('index', {tasks : tsks});
+    });	
 });
 
 app.listen(port); 

eba59b5de8b98f71ff6db6cc9fc4405501546b07
klsnreddy <klsnreddy@gmail.com>
2016-01-31 04:06:49 +0000

    Connected to MongoDB

--- public/js/app.js
+++ public/js/app.js
@@ -8,16 +8,16 @@ function ctrlFunc() {
 }*/
 
 // This code only runs on the client
-
   angular.module('simple-todos',[]);
  
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
     function ($scope) {
- 
-      $scope.tasks = [
+      $scope.tasks = tasks;
+     /*
+ $scope.tasks = [
         { text: 'This is task 1' },
         { text: 'This is task 2' },
         { text: 'This is task 3' }
       ];
- 
+*/
   }]);
